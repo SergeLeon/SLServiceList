298b34fe6ae51f55301cc3740b65309aa0f61170
Sergey Kravchenko <g605413@gmail.com>
2023-01-11 17:58:00 +0300

    Added home page

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.templatetags.static import static
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('favicon.ico.', RedirectView.as_view(url=static('favicon.ico'))),
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'))),
+    path('', lambda request: render(request, "index.html")),
     path('r/', include("apps.cutter.urls")),
     path('c/', include("apps.converter.urls")),
 ]
